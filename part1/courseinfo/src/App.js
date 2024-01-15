1369c17204a479a9578e4e0e91e6ab3f4532c372
Tony_Xia <Tony_Xia@Dell.com>
2024-01-15 12:53:35 +0800

    1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -37,18 +38,26 @@ const Total = (props) => {
 
 
 const App = () => {
+
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
       <Header course={course} />
-      <Content part1name={part1} part1num={exercises1} part2name={part2} part2num={exercises2} part3name={part3} part3num={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1name={part1.name} part1num={part1.exercises} part2name={part2.name} part2num={part2.exercises} part3name={part3.name} part3num={part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
