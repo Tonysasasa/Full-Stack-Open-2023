32ab5c487989eaffc1ad751cb1cc52541d97df18
Tony_Xia <Tony_Xia@Dell.com>
2024-02-19 14:52:10 +0800

    1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,10 +1,29 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const all = props.good + props.bad + props.neutral;
+  console.log(props.good);
+  const average = (props.good - props.bad) / (props.good + props.bad + props.neutral)
+  const positive = props.good / (props.good + props.bad + props.neutral) * 100
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
 
   return (
     <div>
@@ -16,12 +35,7 @@ const App = () => {
 
       <h1>statistics</h1>
 
-      <p>good {good} </p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>average {(good - bad) / (good + bad + neutral)}</p>
-      <p>positive {good / (good + bad + neutral) * 100} %</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
 
     </div>
   )
