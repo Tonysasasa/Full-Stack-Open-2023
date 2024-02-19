3bab8d682544c6a102d2cbb6bde6ee9b82d8e84c
Tony_Xia <Tony_Xia@Dell.com>
2024-02-19 15:29:07 +0800

    remove warning on console

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -19,14 +19,16 @@ const Statistics = (props) => {
     )
   }
   return (
-    <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
   )
 }
 
@@ -43,9 +45,9 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  // const handleGoodClick = () => { setGood(good + 1) }
-  // const handleNeutralClick = () => { setNeutral(neutral + 1) }
-  // const handleBadClick = () => { setBad(bad + 1) }
+  // const handleGoodClick = () => {setGood(good + 1)}
+  // const handleNeutralClick = () => {setNeutral(neutral + 1)}
+  // const handleBadClick = () => {setBad(bad + 1)}
 
   return (
     <div>
