6235813851c04de52b83650ffa3f92f89f08f438
Tony_Xia <Tony_Xia@Dell.com>
2024-02-20 15:48:01 +0800

    1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,28 +1,48 @@
 import { useState } from 'react'
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+]
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const arrHelper = (arr, index) => {
+  arr[index] += 1
+}
+
+const points = new Uint8Array(anecdotes.length);
+const copy = [...points]
+
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-
   const [selected, setSelected] = useState(0)
-  console.log(selected)
+  const [vote, setVote] = useState(0)
+
+  const handleVoteClick = () => {
+    setVote(vote + 1);
+    arrHelper(copy, selected);
+    //console.log(copy);
+  }
 
   const handleButtonClick = () => { setSelected(Math.floor(Math.random() * anecdotes.length)) }
 
   return (
     <div>
-      <p>
-        {anecdotes[selected]}
-      </p>
-      <button onClick={handleButtonClick}>next anecdotes</button>
+      <p>{anecdotes[selected]}</p>
+      <p>has {copy[selected]} votes</p>
+
+      <Button onClick={handleVoteClick} text="vote" />
+      <Button onClick={handleButtonClick} text="next anecdotes" />
     </div>
   )
 }
