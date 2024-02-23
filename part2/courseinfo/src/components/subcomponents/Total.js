bd20de65cac66ee7b4bf6062cefbf35508a5aef3
Tony_Xia <Tony_Xia@Dell.com>
2024-02-23 12:01:31 +0800

    2.3

--- part2/courseinfo/src/components/subcomponents/Total.js
+++ part2/courseinfo/src/components/subcomponents/Total.js
@@ -2,9 +2,15 @@ import React from 'react'
 import Part from './Part'
 let sum = 0;
 const Total = ({ total }) => {
-    total.forEach((element) => (sum += element.exercises));
+    // total.forEach((element) => (sum += element.exercises));
+    // return (
+    //     <p><b>total of {sum} exercises</b></p>
+    // )
+
+    const val =
+        total.reduce((s, p) => s + p.exercises, 0)
     return (
-        <p><b>total of {sum} exercises</b></p>
+        <p>Number of exercises {val}</p>
     )
 }
 export default Total
